e9a878fc2129450035342e727c302c6c19411e51
AnabolicHippo <slforward@creeradio.com>
2019-05-16 21:44:31 -0400

    First Commit

---
+++ flask-app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow 
+import os
+
+# Init App
+app = Flask(__name__)
+
+@app.route('/', methods={'GET'})
+def get():
+    return jsonify({'msg': 'hello-world'})
+# Run Server
+if __name__ == '__main__':
+    app.run()
