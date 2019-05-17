d6f1bea0e15bf050f429560a7b6cf958504bbd7f
AnabolicHippo <slforward@creeradio.com>
2019-05-16 22:44:23 -0400

    Completed First Route

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,10 +5,54 @@ import os
 
 # Init App
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/', methods={'GET'})
-def get():
-    return jsonify({'msg': 'hello-world'})
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init db
+db = SQLAlchemy(app)
+#Init ma
+ma = Marshmallow(app)
+#Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+#Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
+#Init Schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
+#Create a Product
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    new_product = Product(name, description, price, qty)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
 # Run Server
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=False)
